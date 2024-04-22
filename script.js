768b9080603edad0a9157417794c5b49d936db71
James Barker <barker.jw@protonmail.com>
2024-04-22 17:10:13 +1000

    Feat: Add score variables

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice () {
     const pick = ['Rock' , 'Paper' , 'Scissors']
     return pick[Math.floor(Math.random () * pick.length)];
 }
-
+// OLD CODE USED FOR COMPUTER CHOICE - LEAVING FOR REVISION
 // let result = getComputerChoice(1, 3);
 
 //     if (result === 1){
@@ -15,16 +15,18 @@ function getComputerChoice () {
 //     console.log("Scissors")
 //    }
 
-function getHumanChoice() {
-    var choice = prompt("Make your move!" , "Rock, Paper or Scissors")
+// function getHumanChoice() {
+//     var choice = prompt("Make your move!" , "Rock, Paper or Scissors")
 
-   }
-
+//    }
 
 let humanScore = 0
 let computerScore = 0
 
+
 function playRound(humanSelection, computerSelection) {
+
+    
 
     if (humanSelection === computerSelection) {
         return `It's a draw! you both picked ${humanSelection}`;
@@ -32,21 +34,29 @@ function playRound(humanSelection, computerSelection) {
     } 
     
     else if (humanSelection === "Rock" && computerSelection === "Scissors") {
+        humanScore++;
         return `You win! ${humanSelection} beats ${computerSelection}`;
+         
 
     } else if (humanSelection === "Scissors" && computerSelection === "Paper") {
+        humanScore++;
         return `You win! ${humanSelection} beats ${computerSelection}`;
+        
     } 
     else if (humanSelection === "Paper" && computerSelection === "Rock") {
+        humanScore++;
         return `You win! ${humanSelection} beats ${computerSelection}`;
+        
     }
 
     else {
+        computerScore++;
     return `You lose! ${computerSelection} beats ${humanSelection}`;
+    
 
     }
  
-
+// OLD CODE USED FOR PLAY ROUND
     // if (computerSelection === "Rock" && humanSelection === "Paper") {
     //     return("You win!")
     // }
@@ -65,3 +75,5 @@ const humanSelection = prompt("Make your move!" , "Rock, Paper or Scissors")
 const computerSelection = getComputerChoice();
 
 console.log(playRound(humanSelection, computerSelection));
+
+console.log(humanScore);
